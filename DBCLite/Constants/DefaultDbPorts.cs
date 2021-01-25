dc5e28277938e777d6f27983abd96ad3f17ba794
João Neves <joao95neves@gmail.com>
2021-01-25 12:22:14 +0000

    Multiple rafactorings to the DBConnectionString model

---
+++ DBCLite/Constants/DefaultDbPorts.cs
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020 João Pedro Martins Neves (SHIVAYL) - All Rights Reserved.
+ * https://github.com/joao-neves95
+ *
+ * DBCLite and all its content are licensed under the GNU AFFERO GENERAL PUBLIC LICENSE v3.0
+ * (AGPL-3.0), located in the root folder, under the name "LICENSE.md".
+ *
+ */
+
+namespace DBCLite.Constants
+{
+    public static class DefaultDbPorts
+    {
+        public const string POSTGRESQL = "5432";
+
+        public const string SQL_SERVER = "1433";
+
+        public const string MYSQL = "3306";
+    }
+}
--- DBCLite/Models/DBConnectionString.cs
+++ DBCLite/Models/DBConnectionString.cs
@@ -7,22 +7,54 @@
  *
  */
 
+using DBCLite.Constants;
+
 using System;
 
 namespace DBCLite.Models
 {
     /// <summary>
-    ///
     /// Representation of the connection string.
     ///
     /// </summary>
     /// <see cref="https://www.connectionstrings.com/"/>
     public class DBConnectionString
     {
+        #region CONSTRUCTORS
+
+        public DBConnectionString()
+        {
+        }
+
+        public DBConnectionString(string serverAddress, string port, string database, string user, string password)
+        {
+            this.Init( serverAddress, port, database, user, password );
+        }
+
+        public DBConnectionString(string serverAddress, string database, string user, string password)
+        {
+            this.Init( serverAddress, null, database, user, password );
+        }
+
+        private void Init(string serverAddress, string port, string database, string user, string password)
+        {
+            this.ServerAddress = serverAddress;
+            this.Port = port;
+            this.Database = database;
+            this.User = user;
+            this.Password = password;
+        }
+
+        #endregion CONSTRUCTORS
+
         #region PROPERTIES
 
         public string ServerAddress { get; set; }
 
+        /// <summary>
+        /// If <see langword="null"/>, the <see cref="DefaultDbPorts"/> are used.
+        ///
+        /// </summary>
         public string Port { get; set; }
 
         public string Database { get; set; }
@@ -35,27 +67,68 @@ namespace DBCLite.Models
 
         #region METHODS
 
-        #region PostgreSQL
+        #region PostgreSQL - Npgsql
 
-        public string ToPostreSQLConnectionString()
+        public string ToNpgsqlConnectionString()
         {
+            if (string.IsNullOrEmpty( this.Port ))
+            {
+                this.Port = DefaultDbPorts.POSTGRESQL;
+            }
+
             return $"Server={this.ServerAddress}; Port={this.Port}; Database={this.Database}; User Id={this.User}; Password={this.Password};";
         }
 
-        public string BuildPostreSQLConnectionStringWithSSL()
+        public string ToNpgsqlConnectionStringWithoutSSL()
         {
-            return this.ToPostreSQLConnectionString() + " SslMode=Require;";
+            return this.ToNpgsqlConnectionString() + this.NpgsqlWithoutSSL();
         }
 
-        #endregion PostgreSQL
+        public string ToNpgsqlConnectionStringWithSSL()
+        {
+            return this.ToNpgsqlConnectionString() + this.NpgsqlWithSSL();
+        }
+
+        public string ToNpgsqlConnectionStringWithPoolingControl(int maxPoolSize, bool ssl = true, int minPoolSize = 1)
+        {
+            return (ssl ? this.ToNpgsqlConnectionStringWithSSL() : this.ToNpgsqlConnectionStringWithoutSSL()) +
+                   $" Pooling=true; MinPoolSize={minPoolSize}; MaxPoolSize={maxPoolSize};";
+        }
+
+        private string NpgsqlWithSSL()
+        {
+            return " Protocol=3; SSL=true; SslMode=Require;";
+        }
+
+        private string NpgsqlWithoutSSL()
+        {
+            return " Protocol=3; SSL=false; SslMode=Disable;";
+        }
+
+        #endregion PostgreSQL - Npgsql
 
         #region MySQL
 
+        /// <summary>
+        /// NOT TESTED.
+        ///
+        /// </summary>
+        /// <returns></returns>
         public string ToMySQLConnectionString()
         {
+            if (string.IsNullOrEmpty( this.Port ))
+            {
+                this.Port = DefaultDbPorts.MYSQL;
+            }
+
             return $"Server={this.ServerAddress}; Database={this.Database}; Uid={this.User}; Pwd={this.Password};";
         }
 
+        /// <summary>
+        /// NOT TESTED.
+        ///
+        /// </summary>
+        /// <returns></returns>
         public string ToMySQLConnectionStringWithSSL()
         {
             return this.ToMySQLConnectionString() + " SslMode=Required;";
@@ -65,22 +138,42 @@ namespace DBCLite.Models
 
         #region SQLServer
 
+        /// <summary>
+        /// NOT TESTED.
+        ///
+        /// </summary>
+        /// <returns></returns>
         public string ToSQLServerConnectionString()
         {
+            if (string.IsNullOrEmpty( this.Port ))
+            {
+                this.Port = DefaultDbPorts.SQL_SERVER;
+            }
+
             return $@"
                 Server={
                     this.ServerAddress +
                     (
-                        !String.IsNullOrEmpty(this.Port) ? $",{this.Port}" : ""
+                        !String.IsNullOrEmpty( this.Port ) ? $",{this.Port}" : ""
                     )
                 }; Database={this.Database}; User Id={this.User}; Password={this.Password};";
         }
 
+        /// <summary>
+        /// NOT TESTED.
+        ///
+        /// </summary>
+        /// <returns></returns>
         public string ToSQLServerTrustedConnectionString()
         {
             return this.ToSQLServerConnectionString() + " Trusted_Connection=True;";
         }
 
+        /// <summary>
+        /// NOT TESTED.
+        ///
+        /// </summary>
+        /// <returns></returns>
         public string ToSQLServerIPConnectionString()
         {
             return $"Data Source={this.ServerAddress}; Network Library=DBMSSOCN; Initial Catalog={this.Database}; User ID={this.User}; Password={this.Password};";
